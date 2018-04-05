0d8d6c62f5ae7edbc0b5587591a98b5b0e5fbce2
Kent <kentsaeteurn@gmail.com>
2018-04-05 16:06:34 -0700

    add routing to landing and library page/components

---
+++ src/components/Library.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Library extends React.Component {
+  render () {
+    return (
+      <div>
+        This is the library page
+      </div>
+    )
+  }
+}
+
+export default Library;

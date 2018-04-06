32a5f85b33406957e1945c6a308c06155d7de09c
Kent <kentsaeteurn@gmail.com>
2018-04-06 13:57:23 -0700

    add meaningful content to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,22 @@ class Landing extends React.Component {
   render () {
     return (
       <div>
-        This is the landing page
+        <section className="library">
+          <section className="selling-points">
+            <div className="point">
+              <h2 className="point-title">Choose your music</h2>
+              <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+            </div>
+            <div className="point">
+              <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+              <p className="point-description">No arbitrary limits. No distractions.</p>
+            </div>
+            <div className="point">
+              <h2 className="point-title">Mobile enabled</h2>
+              <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+            </div>
+          </section>
+        </section>
       </div>
     )
   }

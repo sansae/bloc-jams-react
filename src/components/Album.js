10954d867fbcc733e6c1474cd74315139992ed49
Kent <kentsaeteurn@gmail.com>
2018-04-08 14:31:57 -0700

    add library, add url parameter to display different albums in library

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,9 +3,9 @@ import React from 'react';
 class Album extends React.Component {
   render () {
     return (
-      <div>
-      
-      </div>
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
     )
   }
 }
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
 class Library extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { albums: albumData };
+  }
+
   render () {
     return (
-      <div>
-        This is the library page
-      </div>
+      <section className="library">
+        {
+          this.state.albums.map((album, index) =>
+            <Link to={`/album/${album.slug}`} key={index}>
+              <img src={album.albumCover} alt={album.title} />
+              <div>{album.title}</div>
+              <div>{album.artist}</div>
+              <div>{album.songs.length} songs</div>
+            </Link>
+          )
+        }
+      </section>
     )
   }
 }

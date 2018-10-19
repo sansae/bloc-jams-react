f92c9a7a109db63ee062d691c7e1f40968f06318
Kent <kentsaeteurn@gmail.com>
2018-10-19 07:04:26 -0700

    prevent app from sleeping in production

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const http = require("http");
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,3 +12,7 @@ ReactDOM.render(
   </BrowserRouter>
   , document.getElementById('root'));
 registerServiceWorker();
+
+setInterval(() => {
+  http.get("http://bloc-jams-react-kesa.herokuapp.com/");
+}, 300000);
